b18fee021324a92ab011e1563c664fe1d743c768
SHARAN M <121308881+SHARAN-7477@users.noreply.github.com>
2023-01-29 06:32:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function shar(){
+    var a = document.getElementsByClassName("result")[0].value;
+    document.getElementsByClassName("result")[0].value=(eval(a));
+}
+
+function kugan(){
+    document.getElementsByClassName("result")[0].value='';
+
+}
